65da2759622c65b91867a721fb06152f7fa16f2d
tilliedstrom <tilliedstrom@elev.ga.ntig.se>
2025-12-08 11:18:41 +0100

    collision colors

--- src/Game.js
+++ src/Game.js
@@ -13,7 +13,17 @@ export default class Game {
 
         // Skapa alla objekt i spelet
         this.gameObjects = [
-            new Rectangle(this, 200, 150, 50, 50, 'red')
+            new Rectangle(this, 220, 150, 25, 50, 'red'),
+            new Rectangle(this, 300, 150, 25, 50, 'purple'),
+            new Rectangle(this, 300, 200, 15, 300, 'purple'),
+            new Rectangle(this, 230, 200, 15, 300, 'purple'),
+            new Rectangle(this, 0, 200, 230, 15, 'purple'),
+            new Rectangle(this, 220, 0, 15, 90, 'purple'),
+
+            new Rectangle(this, 0, 0, 15, 480, 'purple'),
+            new Rectangle(this, 840, 0, 15, 480, 'purple'),
+            new Rectangle(this, 0, 0, 840, 15, 'purple'),
+            new Rectangle(this, 0, 470, 840, 15, 'purple')
         ]
 
         // Sätt starthastighet (pixlar per millisekund)
@@ -37,6 +47,13 @@ export default class Game {
         }
 
         this.gameObjects.forEach(obj => {
+            var playerColor = this.player.color
+            var objColor = obj.color
+            if (this.player.intersects(obj)) {
+                console.log("Intersect")
+                this.player.color = objColor
+                obj.color = playerColor
+            }
             if (obj !== this.player && this.player.intersects(obj)) {
                 // Hantera kollision baserat på riktning
                 if (this.player.directionX > 0) { // rör sig åt höger
